29899415fc386a140fb0cece9ff2f2ffbf49790f
Lck <kle995301@gmail.com>
2024-05-24 18:06:10 +0700

    test project tool

---
+++ src/main/java/tool/tool.java
@@ -0,0 +1,13 @@
+package tool;
+
+import java.awt.PageAttributes.MediaType;
+
+import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+
+public class tool {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
